5eafc8e53a85a4cc185992033bb2f796d749b5e9
stevehon12 <stevehong124@gmail.com>
2025-09-02 16:15:47 +0900

    Add untracked files

---
+++ vue-project/src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,35 @@
+<template>
+  <div >
+    <h1>{{ reactiveMessage }}</h1>
+    <button v-on:click="addReactiveMessage">Add m</button>
+    <h2>{{normalMessage}}</h2>
+    <button v-on:click ="addNormalMessage">Add m</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const reactiveMessage = ref('Hello Vue 3!');
+    const addReactiveMessage = ()=>{
+      reactiveMessage.value = reactiveMessage.value + '!';
+    };
+
+    let normalMessage = 'hello';
+    const addNormalMessage =() => {
+      normalMessage = normalMessage + '!'
+    };
+    return {
+      reactiveMessage,
+      normalMessage,
+      addNormalMessage,
+      addReactiveMessage,
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue-project/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import AppCompositionApi from './lectures/4/App_CompositionAPI.vue'
+
+createApp(AppCompositionApi).mount('#app')
